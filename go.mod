c262051e939e02f07bd3e8166cdb257f5e44dfcd
iBakuman <903926441@qq.com>
2024-06-12 11:50:14 +0800

    context.WithValue

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.22
 
 require (
 	github.com/lib/pq v1.3.0
+	github.com/robfig/cron/v3 v3.0.1
+	github.com/spf13/cobra v1.8.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.9.0
 	github.com/testcontainers/testcontainers-go v0.31.0
 	github.com/tnclong/go-que v0.0.0-20240226030728-4e1f3c8ec781
+	golang.org/x/net v0.21.0
 	golang.org/x/sys v0.20.0
 	golang.org/x/time v0.3.0
 	gorm.io/driver/postgres v1.5.7
@@ -55,11 +58,9 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
---
+++ stdlib/context/context_test.go
@@ -0,0 +1,115 @@
+package context
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
+)
+
+func TestValue(t *testing.T) {
+	type contextKey int
+	const (
+		key1 contextKey = 1
+		key2            = 2
+		key3            = 3
+	)
+	newContext := func(ctx context.Context, k contextKey, v any, convert bool) context.Context {
+		if convert {
+			return context.WithValue(ctx, int(k), v)
+		} else {
+			return context.WithValue(ctx, k, v)
+		}
+	}
+	fromContext := func(ctx context.Context, k contextKey, convert bool) any {
+		if convert {
+			return ctx.Value(int(k))
+		} else {
+			return ctx.Value(k)
+		}
+	}
+
+	type person struct {
+		Name string
+		Age  int
+	}
+
+	t.Run("plain", func(t *testing.T) {
+		ctx := context.WithValue(context.Background(), "key", "value")
+		require.Equal(t, "value", ctx.Value("key"))
+		require.Nil(t, ctx.Value("not-exist"))
+		require.Panics(t, func() {
+			_ = ctx.Value("non-exist").(*person)
+		})
+		v, ok := ctx.Value("non-exist").(*person)
+		require.Nil(t, v)
+		require.False(t, ok)
+	})
+
+	t.Run("stored in contextKey, retrieved with contextKey", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = newContext(ctx, key1, &person{Name: "Alice", Age: 20}, false)
+		require.Equal(t, "Alice", fromContext(ctx, key1, false).(*person).Name)
+	})
+
+	t.Run("stored in int, retrieved with int", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = newContext(ctx, key1, &person{Name: "Alice", Age: 20}, true)
+		require.Equal(t, "Alice", fromContext(ctx, key1, true).(*person).Name)
+	})
+
+	t.Run("stored in contextKey, retrieved with int", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = newContext(ctx, key1, &person{Name: "Alice", Age: 20}, false)
+		require.Nil(t, fromContext(ctx, key1, true))
+	})
+
+	t.Run("stored in int, retrieved with contextKey", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = newContext(ctx, key1, &person{Name: "Alice", Age: 20}, true)
+		require.Nil(t, fromContext(ctx, key1, false))
+	})
+}
+
+func TestValueWithEmptyContext(t *testing.T) {
+	type person struct {
+		Name string
+		Age  int
+	}
+	ctx := context.WithValue(context.Background(), "key", "value")
+	require.Equal(t, "value", ctx.Value("key"))
+	require.Nil(t, ctx.Value("not-exist"))
+	require.Panics(t, func() {
+		_ = ctx.Value("non-exist").(*person)
+	})
+	v, ok := ctx.Value("non-exist").(*person)
+	require.Nil(t, v)
+	require.False(t, ok)
+
+	type e1 struct{}
+	a := e1{}
+	b := e1{}
+	require.True(t, a == b)
+	type e3 struct{}
+	a1 := e3{}
+	var ia any = a
+	var ia1 any = a1
+	require.False(t, ia == ia1)
+
+	type e2 struct {
+		Name string
+	}
+	c := e2{Name: "Alice"}
+	d := e2{Name: "Alice"}
+	require.True(t, c == d)
+	var i1 any = c
+	var i2 any = d
+	require.True(t, i1 == i2)
+
+	c = e2{Name: "Alice"}
+	d = e2{Name: "Bob"}
+	require.False(t, c == d)
+	i1 = c
+	i2 = d
+	require.False(t, i1 == i2)
+}

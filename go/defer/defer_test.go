1319561a3f074d5d088f3d1e6ac9bd16f40d63ab
iBakuman <903926441@qq.com>
2024-07-15 16:19:35 +0800

    panic

--- go/defer/defer_test.go
+++ go/defer/defer_test.go
@@ -1,6 +1,12 @@
 package _defer
 
-import "testing"
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestDefer(t *testing.T) {
 	fn := func() {
@@ -10,4 +16,29 @@ func TestDefer(t *testing.T) {
 		}()
 	}
 	fn()
+}
+
+func TestDefer2(t *testing.T) {
+	t.Run("return value from recover should be string", func(t *testing.T) {
+		defer func() {
+			recovered := recover()
+			ty := reflect.TypeOf(recovered)
+			require.Equal(t, reflect.TypeOf(""), ty)
+		}()
+		err := "error"
+		panic(err)
+	})
+
+	t.Run("return value from recover should be error", func(t *testing.T) {
+		defer func() {
+			recovered := recover()
+			_, ok := recovered.(error)
+			require.True(t, ok)
+			ty := reflect.TypeOf(recovered)
+			require.NotEqual(t, reflect.TypeOf((*error)(nil)).Elem(), ty)
+			require.Equal(t, reflect.TypeOf(errors.New("")), ty)
+		}()
+		err := errors.New("error")
+		panic(err)
+	})
 }

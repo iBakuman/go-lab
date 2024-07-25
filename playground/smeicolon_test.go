82fe706ae865fb1818eb6a5b9e29d0fe60c7ea12
iBakuman <903926441@qq.com>
2024-07-25 14:52:36 +0800

    semicolon

--- playground/smeicolon_test.go
+++ playground/smeicolon_test.go
@@ -1,17 +1,46 @@
 package playground
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func FunA(a int) (int, int) {
 	return 2 * a, 4 * a
 }
 
-func TestSemicolon(t *testing.T) {
+func FunB(a int) (err error) {
+	err, a = errors.New("hello"), 2
+	return
+}
+
+func FunC(t *testing.T, a int) (err error) {
+	errStr := "hello"
+	defer func() {
+		if err == nil {
+			t.Fatal("err is nil")
+		} else {
+			if err.Error() != errStr {
+				t.Fatal("err is not hello")
+			}
+		}
+		err = nil
+	}()
+	err, b := errors.New(errStr), 2
+	t.Logf("a: %d, b: %d", a, b)
+	return
+}
+
+func TestSemicolonFuncA(t *testing.T) {
 	a := 4
 	t.Logf("&a: %p", &a)
 	a, b := FunA(a)
 	t.Logf("&a: %p", &a)
 	t.Logf("a: %d, b: %d", a, b)
+}
+
+func TestSemicolonFuncC(t *testing.T) {
+	require.NoError(t, FunC(t, 2))
 }
